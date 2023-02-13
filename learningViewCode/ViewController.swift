4a2fd17e6fcbabc16638f4f1cc1099bc6ec40cf5
Vitor Gomes <vitorgomes_silva@outlook.com>
2023-02-13 20:52:49 -0300

    Initial commit, setting SceneDelegate and first test

---
+++ learningViewCode/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  learningViewCode
+//
+//  Created by Vitor Gomes on 12/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Testando se funcionou as primeiras configurações de ViewCode
+        view.backgroundColor = .red
+    }
+
+
+}
+
